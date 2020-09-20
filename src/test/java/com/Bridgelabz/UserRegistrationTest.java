b2d823044a2455c9f76eac86e14b2971eff877d9
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-20 23:27:15 +0530

    [Saurabh]Modified: Validate last name with false method

--- src/test/java/com/Bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/Bridgelabz/UserRegistrationTest.java
@@ -14,9 +14,33 @@ public class UserRegistrationTest {
     }
 
     @Test
+    public void FirstName_havingMinimum3char_returnFalse() {
+        UserDetails userDetails = new UserDetails();
+        boolean result = userDetails.checkFirstName("saurabh");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void FirstName_havingMinimum3char_returnFalse2() {
+        UserDetails userDetails = new UserDetails();
+        boolean result = userDetails.checkFirstName("Sa");
+        Assert.assertEquals(false, result);
+    }
+
+
+    @Test
     public void LastName_havingMinimum3char_returnTrue() {
         UserDetails userDetails = new UserDetails();
         boolean result = userDetails.checkLastName("Dagwar");
         Assert.assertEquals(true,result);
     }
+
+    @Test
+    public void LastName_havingMinimum3char_returnFalse() {
+        UserDetails userDetails = new UserDetails();
+        boolean result = userDetails.checkLastName("dagwar");
+        Assert.assertEquals(false,result);
+    }
+
+
 }
