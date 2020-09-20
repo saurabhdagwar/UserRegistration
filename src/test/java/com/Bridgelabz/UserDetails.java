3b45d8b2bc6b278870fd888d411b0aeea6e9c8de
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-20 13:28:50 +0530

    [Saurabh]Added: Validate Email having proper format

--- src/test/java/com/Bridgelabz/UserDetails.java
+++ src/test/java/com/Bridgelabz/UserDetails.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public class UserDetails {
     public static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([._+#][a-zA-Z0-9]+)*@[a-zA-Z1-9]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,2})?$";
 
     public boolean checkFirstName(String FirstName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -13,5 +14,10 @@ public class UserDetails {
     public boolean checkLastName(String LastName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(LastName).matches();
+    }
+
+    public boolean checkEmail(String email) {
+    Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+    return pattern.matcher(email).matches();
     }
 }
--- src/test/java/com/Bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/Bridgelabz/UserRegistrationTest.java
@@ -3,20 +3,28 @@ package com.Bridgelabz;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Scanner;
 
 public class UserRegistrationTest {
     @Test
     public void FirstName_havingMinimum3char_returnTrue() {
         UserDetails userDetails = new UserDetails();
         boolean result = userDetails.checkFirstName("Saurabh");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
 
     @Test
     public void LastName_havingMinimum3char_returnTrue() {
         UserDetails userDetails = new UserDetails();
         boolean result = userDetails.checkLastName("Dagwar");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
+
+    @Test
+    public void Email_havingValidPattern_returnTrue() {
+        UserDetails userDetails = new UserDetails();
+        boolean result = userDetails.checkEmail("abc.xyz@bl.co.in");
+        Assert.assertEquals(true, result);
+    }
+
+
 }
